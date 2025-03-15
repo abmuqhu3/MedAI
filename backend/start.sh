4886df5dcc86db4f2a20e0627a5803f745a25be7
abmuqhu3 <am586288@gmail.com>
2025-03-15 14:59:46 +0530

    Removed backend submodule and fixed structure

---
+++ backend/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python -m waitress --host 0.0.0.0 --port 10000 backend.scripts.main:app
--- start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-waitress-serve --host=0.0.0.0 --port=$PORT backend.scripts.main:app
